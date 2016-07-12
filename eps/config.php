b9af45eda7a282b95cdf688c3b511bcfbd318011
Ben Towner <ben.towner@equinix.com>
2016-07-12 05:50:14 -0400

    add config

--- eps/config.php
+++ eps/config.php
@@ -1,7 +1,7 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://localhost/eps/eps/');
-define('HTTP_CATALOG', 'http://localhost/eps/');
+define('HTTP_SERVER', 'http://localhost/eps/eps');
+define('HTTP_CATALOG', 'http://localhost/eps/eps');
 define('HTTP_HELP', 'http://localhost/eps/eps/');
 
 // HTTPS
@@ -10,23 +10,23 @@ define('HTTPS_CATALOG', 'http://localhost/eps/eps/');
 
 // DIR
 define('DIR_APPLICATION', '/usr/share/eps/eps/');
-define('DIR_SYSTEM', '/usr/share/eps/eps/system/');
-define('DIR_DATABASE', '/usr/share/eps/eps/system/database/');
-define('DIR_LANGUAGE', '/usr/share/eps/eps/eps/language/');
-define('DIR_TEMPLATE', '/usr/share/eps/eps/eps/view/template/');
-define('DIR_CONFIG', '/usr/share/eps/eps/system/config/');
-define('DIR_IMAGE', '/usr/share/eps/eps/image/');
-define('DIR_CACHE', '/usr/share/eps/eps/system/cache/');
-define('DIR_DOWNLOAD', '/usr/share/eps/eps/download/');
-define('DIR_LOGS', '/usr/share/eps/eps/system/logs/');
-define('DIR_CATALOG', '/usr/share/eps/eps/catalog/');
-define('DIR_UPLOAD', '/usr/share/eps/eps/system/upload/');
-define('DIR_MODIFICATION', '/usr/share/eps/eps/system/modification/');
+define('DIR_SYSTEM', '/usr/share/eps/system/');
+define('DIR_DATABASE', '/usr/share/eps/system/database/');
+define('DIR_LANGUAGE', '/usr/share/eps/eps/language/');
+define('DIR_TEMPLATE', '/usr/share/eps/eps/view/template/');
+define('DIR_CONFIG', '/usr/share/eps/system/config/');
+define('DIR_IMAGE', '/usr/share/eps/image/');
+define('DIR_CACHE', '/usr/share/eps/system/cache/');
+define('DIR_DOWNLOAD', '/usr/share/eps/download/');
+define('DIR_LOGS', '/usr/share/eps/system/logs/');
+define('DIR_CATALOG', '/usr/share/eps/catalog/');
+define('DIR_UPLOAD', '/usr/share/eps/system/upload/');
+define('DIR_MODIFICATION', '/usr/share/eps/system/modification/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'eps');
+define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'eps2!');
 define('DB_DATABASE', 'eps');
 define('DB_PREFIX', 'eps_');
